1ab343e0d58603d04284f3f2d6809102a2965631
Lewin Bormann <lbo@spheniscida.de>
2017-09-02 19:52:31 +0200

    Feature: Name timers to allow for several timers per case.

--- example/src/example.rs
+++ example/src/example.rs
@@ -7,4 +7,38 @@ mod tests {
         assert!(true);
         assert_eq!(1, 1);
     }
+
+    #[test]
+    fn my_test_with_initialization() {
+        // Do possibly expensive preparation before.
+        let a = 1;
+        let b = 2;
+
+        time_test!();
+
+        assert!(true);
+        assert_eq!(1, 1);
+    }
+
+    #[test]
+    fn my_test_with_multiple_timers() {
+        time_test!();
+
+        {
+            time_test!("all");
+            {
+                // sub-test 1
+                time_test!("sub1");
+                assert_eq!(1, 1);
+            }
+
+            {
+                // sub-test 2
+                time_test!("sub2");
+                assert!(true);
+            }
+        }
+
+        // The order of timers will be sub1, sub2, all, <unnamed>.
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,22 @@ use std::io::{self, Write};
 extern crate time;
 
 /// TestTimer allows to easily time tests. It's recommended to use the time_test!() macro.
-pub struct TestTimer(time::Timespec);
+pub struct TestTimer(time::Timespec, String);
 
 impl TestTimer {
-    pub fn new() -> TestTimer {
-        TestTimer(time::get_time())
+    pub fn new(desc: String) -> TestTimer {
+        TestTimer(time::get_time(), desc)
     }
 }
 
 impl Drop for TestTimer {
     fn drop(&mut self) {
-        write!(io::stderr(), "(took {}) ", time::get_time() - self.0).is_ok();
+        let dur = time::get_time() - self.0;
+        if self.1.is_empty() {
+            write!(io::stderr(), "(took {}) ", dur).is_ok();
+        } else {
+            write!(io::stderr(), "({} took {}) ", self.1, dur).is_ok();
+        }
     }
 }
 
@@ -21,6 +26,10 @@ impl Drop for TestTimer {
 macro_rules! time_test {
     () => {
         use time_test::TestTimer;
-        let _tt = TestTimer::new();
+        let _tt = TestTimer::new(String::new());
+    };
+    ($desc:expr) => {
+        use time_test::TestTimer;
+        let _tt = TestTimer::new($desc.to_string());
     }
 }
